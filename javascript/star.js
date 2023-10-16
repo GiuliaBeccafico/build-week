9ae8abb87f83b040b14477552581fdd1d6cfbc23
adrianobruzzese <ad.bruzzese@gmail.com>
2023-10-16 20:31:30 +0200

    updated-layout

---
+++ javascript/star.js
@@ -0,0 +1,9 @@
+const target = document.getElementById('rating')
+const stars = target.querySelectorAll('.star')
+
+for (let star of stars){
+    star.addEventListener('mouseover', function (){
+        star.classList.toggle('star')
+    })
+   
+}
