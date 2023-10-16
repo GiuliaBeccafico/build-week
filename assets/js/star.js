8d2ab80325d478c033d9df72db823ff1073fc1e8
Maudit00 <davide9311@gmail.com>
2023-10-16 17:28:51 +0200

    class toggle funziona

--- assets/js/star.js
+++ assets/js/star.js
@@ -1,9 +1,10 @@
 const target = document.getElementById('rating')
 const stars = target.querySelectorAll('.star')
 
-console.dir(target, stars)
 for (let star of stars){
     star.addEventListener('mouseover', function (){
-    star.style.fill = '#00FFFF'
+        star.classList.toggle('star')
     })
+   
 }
+
