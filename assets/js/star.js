81ecf0401d941e5a92722c27823378cbcf954506
Maudit00 <davide9311@gmail.com>
2023-10-16 16:57:00 +0200

    prove js per animazione stelle

---
+++ assets/js/star.js
@@ -0,0 +1,8 @@
+const target = document.getElementById(rating)
+const stars = target.querySelectorAll('.star')
+
+for (let star of stars){
+    star.addEventListener('click', function (){
+    
+    })
+}
