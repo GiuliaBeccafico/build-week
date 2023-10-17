191bc3b1c214b6d6849222827eac52350eaec780
Maudit00 <davide9311@gmail.com>
2023-10-17 16:28:07 +0200

    prove2 js su feedback page

--- assets/js/star.js
+++ assets/js/star.js
@@ -1,10 +1,11 @@
 const target = document.getElementById('rating')
 const stars = target.querySelectorAll('.star')
 
-for (let star of stars){
-    star.addEventListener('mouseover', function (){
-        star.classList.toggle('star')
-    })
+// for (let star of stars){
+//     star.addEventListener('mouseover', function (){
+//         star.classList.toggle('star')
+//     })
    
-}
+// }
+
 
