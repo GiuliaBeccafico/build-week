56799ab1afce9e3fae023bd69d7f08c9a82269bd
samuelet23 <samueletoscano@hotmail.com>
2023-10-17 18:23:22 +0200

    passaggio tra la prima pagina e la seconda pagina

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,17 @@
 
     let areaCheckbox = document.getElementById('checkbox');
     let areaButton = document.querySelector('.button');
+
     let firstPage = document.getElementById('page1')
     let secondPage = document.getElementById ('page2')
-    
+
+    let timer = document.querySelector('.timer')    
     areaButton.addEventListener('click', function() {
         if (areaCheckbox.checked) {
-          firstPage.classList.add('hidden') 
+          firstPage.remove()
           secondPage.classList.remove('hidden')
+          timer.classList.remove ('hidden')
+          
         } else {
             // L'utente non ha selezionato la casella di controllo, mostra un messaggio di avviso
             Swal.fire({
