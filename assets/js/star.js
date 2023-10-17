31fd9ae3aaa7bb8489878051b2b01d7e72477a77
Maudit00 <davide9311@gmail.com>
2023-10-17 17:27:00 +0200

    id non univoci fixati

--- assets/js/star.js
+++ assets/js/star.js
@@ -1,11 +1,20 @@
 const target = document.getElementById('rating')
-const stars = target.querySelectorAll('.star')
+const stars = target.querySelectorAll('svg')
 
-// for (let star of stars){
-//     star.addEventListener('mouseover', function (){
-//         star.classList.toggle('star')
-//     })
-   
+stars.forEach()
+// for (let i=0; i< stars.length; i++){
+//     stars[i].addEventListener('click', () => {
+//         for (let j=0; j < stars.length; j++){
+//             i >= j ? stars[i].classList.add('active') : stars[i].classList.remove('active')
+//         }
+//     } )
 // }
 
 
+// stars.forEach((item, index1) => {
+//     item.addEventListener('mouseover', () => {
+//         stars.forEach((star, index2) => {
+//             index1 >= index2 ? star.classList.add('active') : star.classList.remove('.active') 
+//         })
+//     })
+// })
