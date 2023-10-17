4a02cae4342781f5a3152aed9d0b1cfadda5afdc
MarcoZiino96 <141355693+MarcoZiino96@users.noreply.github.com>
2023-10-17 18:24:22 +0200

    rimozionepage1

--- assets/js/benchmark.js
+++ assets/js/benchmark.js
@@ -1,17 +1,17 @@
 let areaCheckbox = document.getElementById('checkbox');
-    let areaButton = document.querySelector('.button');
+let areaButton = document.querySelector('.button');
 
 
-    areaButton.addEventListener('click', function() {
-        if (areaCheckbox.checked) {
-          firstPage.classList.add('hidden') 
-          secondPage.classList.remove('hidden')
-        } else {
-            // L'utente non ha selezionato la casella di controllo, mostra un messaggio di avviso
-            Swal.fire({
-                icon: 'error',
-                title: 'Oops...',
-                text: 'Please accept the agreement to proceed'
-            });
-        }
-    });
+areaButton.addEventListener('click', function () {
+    if (areaCheckbox.checked) {
+        page1.remove()
+        page2.classList.remove('hidden')
+    } else {
+        // L'utente non ha selezionato la casella di controllo, mostra un messaggio di avviso
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Please accept the agreement to proceed'
+        });
+    }
+});
