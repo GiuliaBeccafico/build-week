44f7bf3794805070d76d950760cf98e10352f7b8
GiuliaBeccafico <beccaficogiulia@hotmail.com>
2023-10-18 00:28:36 +0200

    js from page1 to page2

--- assets/js/benchmark.js
+++ assets/js/benchmark.js
@@ -1,11 +1,16 @@
 let areaCheckbox = document.getElementById('checkbox');
 let areaButton = document.querySelector('.button');
 
+let firstPage = document.getElementById('page1')
+let secondPage = document.getElementById('page2')
 
+let timer = document.querySelector('.timer')
 areaButton.addEventListener('click', function () {
     if (areaCheckbox.checked) {
-        firstPage.classList.add('hidden')
+        firstPage.remove()
         secondPage.classList.remove('hidden')
+        timer.classList.remove('hidden')
+
     } else {
         Swal.fire({
             icon: 'error',
