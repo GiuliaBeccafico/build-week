4af4fdcd0e223c4de419aff6f9a8ed4ddca47850
samuelet23 <samueletoscano@hotmail.com>
2023-10-17 17:21:45 +0200

    validation checkbox

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+
+
+    let areaCheckbox = document.getElementById('checkbox');
+    let areaButton = document.querySelector('.button');
+
+    
+    areaButton.addEventListener('click', function() {
+        if (areaCheckbox.checked) {
+          firstPage.classList.add('hidden') 
+          secondPage.classList.remove('hidden')
+        } else {
+            // L'utente non ha selezionato la casella di controllo, mostra un messaggio di avviso
+            Swal.fire({
+                icon: 'error',
+                title: 'Oops...',
+                text: 'Please accept the agreement to proceed'
+            });
+        }
+    });
+    let firstPage = document.getElementById('page1')
+    let secondPage = document.getElementById ('page2')
