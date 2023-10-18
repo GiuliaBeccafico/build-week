31c9db4512d3cc70de4f4a32258042d566d9b610
Maudit00 <davide9311@gmail.com>
2023-10-19 01:33:30 +0200

    template try 2

--- assets/js/questionTemplate.js
+++ assets/js/questionTemplate.js
@@ -4,8 +4,26 @@ proceedButton.addEventListener('click', showQuestions)
 
 function showQuestions () {
     let temp = document.getElementsByTagName('template')[0]
-    let target = document.getElementById('container')
-    console.log(temp);
-    let clon = temp.content.cloneNode(true)
-    target.appendChild(clon)
+    let container = document.getElementById('container')
+    let questions = document.createElement('div')
+    container.append(questions)    
+    questions.classList.add('questions')
+
+    cloneSection(0, container, temp)
+    cloneSection(1, container, temp)
+    cloneSection(2, container, temp)
+}
+
+
+function cloneSection (index, target, temp){
+    const children = temp.content.children[index].cloneNode(true)
+    target.append(children)
+}
+
+function questionTitles (target){
+    let title = document.createElement('p')
+    let content = document.createTextNode('Primo titolo')
+    title.append(content)
+    title.classList.add('mainTitle')
+    target.appendChild(title)
 }
