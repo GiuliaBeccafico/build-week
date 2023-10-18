21c1f254adf16a92e34be52d6eb014e99012236f
Maudit00 <davide9311@gmail.com>
2023-10-18 11:23:38 +0200

    prove star filling

--- assets/js/star.js
+++ assets/js/star.js
@@ -7,8 +7,18 @@ radio.forEach(el => {
     el.classList.add('hidden')
 })
 
+<<<<<<< HEAD
 starsPath.forEach(star => {
     star.addEventListener('click', () => {
         star.classList.toggle('activeStar')
     })
 })
+=======
+// for (let i = 0; i <starsPath.length; i++){
+//      starsPath[i].addEventListener('click', () => {
+//         for (let j = 0; j <= i; j++){
+//             starsPath[j].classList.add('activeStar')
+//         }
+//      })
+// }
+>>>>>>> f86400a (prove star filling)
