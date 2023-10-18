2b16355773fddb16d5f10bd5b7b3f8cf7ee2c93f
Persiano99 <sorenrahimi99@gmail.com>
2023-10-18 10:03:52 +0200

    edit

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const target = document.getElementById('rating')
+const stars = target.querySelectorAll('svg')
+const radio = target.querySelectorAll('input[type=radio]')
+
+radio.forEach(el => {
+    el.classList.add('hidden')
+})
