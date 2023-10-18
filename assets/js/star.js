8a83b362a60288678f79e67b88e1fb5c34de5bae
Maudit00 <davide9311@gmail.com>
2023-10-18 17:45:46 +0200

    script verbose minified

--- assets/js/star.js
+++ assets/js/star.js
@@ -13,59 +13,47 @@ radio.forEach(el => {
 for (let i = 0; i < starPath.length; i++){
     starPath[i].index = i
     starPath[i].addEventListener('click', () => {
-        for (let j = 0; j < starPath.length; j++)
-        if (j <= starPath[i].index){
-            starPath[j].classList.remove('emptyStar')
-        } else starPath[j].classList.add('emptyStar')
-    })
+        starFiller(i, starPath)
+        })
+        starPath[i].addEventListener('mouseover', () => {
+          starFiller(i, starPath)
+        })
 }
 
-for (let i = 0; i < starPath.length; i++){
-    starPath[i].index = i
-    starPath[i].addEventListener('mouseover', () => {
-        for (let j = 0; j < starPath.length; j++)
-        if (j <= starPath[i].index){
-            starPath[j].classList.remove('emptyStar')
-        } else starPath[j].classList.add('emptyStar')
-    })
+function starFiller (index, arr){
+  for (let j = 0; j < arr.length; j++)
+  if (j <= arr[index].index){
+      arr[j].classList.remove('emptyStar')
+  } else arr[j].classList.add('emptyStar')
+
 }
 
-// for (let i = 0; i < starPath.length; i++){
-//     starPath[i].index = i
-//     starPath[i].addEventListener('click', starFiller)
-// }
-
-// function starFiller (){
-//     for (let j = 0; j < this.length; j++)
-//         console.log(this.index, this.classList.value());
-//         if (j <= this.index){
-//             this.classList.remove('emptyStar')
-//         } else this.classList.add('emptyStar')
-// }
 const stars = document.querySelectorAll(".star");
 const rating = {
   value: 0,
 };
 
-stars.forEach((star) => {
-  star.addEventListener("mouseover", (event) => {
-    const hoveredStarIndex = parseInt(event.target.getAttribute("data-index"));
-    updateStars(hoveredStarIndex);
-  });
 
-  star.addEventListener("click", (event) => {
-    const clickedStarIndex = parseInt(event.target.getAttribute("data-index"));
-    rating.value = clickedStarIndex + 1;
-    updateStars(clickedStarIndex);
-  });
-});
+/************ versione adriano */
+// stars.forEach((star) => {
+//   star.addEventListener("mouseover", (event) => {
+//     const hoveredStarIndex = parseInt(event.target.getAttribute("data-index"));
+//     updateStars(hoveredStarIndex);
+//   });
 
-function updateStars(hoveredStarIndex) {
-  stars.forEach((star, index) => {
-    if (index <= hoveredStarIndex) {
-      star.src = "assets/img/star.svg";
-    } else {
-      star.src = "assets/img/emptyStar.svg";
-    }
-  });
-}
+//   star.addEventListener("click", (event) => {
+//     const clickedStarIndex = parseInt(event.target.getAttribute("data-index"));
+//     rating.value = clickedStarIndex + 1;
+//     updateStars(clickedStarIndex);
+//   });
+// });
+
+// function updateStars(hoveredStarIndex) {
+//   stars.forEach((star, index) => {
+//     if (index <= hoveredStarIndex) {
+//       star.src = "assets/img/star.svg";
+//     } else {
+//       star.src = "assets/img/emptyStar.svg";
+//     }
+//   });
+// }
