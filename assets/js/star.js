b74864558c05cc0d88136f976097f5aa0b3dfe07
Maudit00 <davide9311@gmail.com>
2023-10-18 10:15:32 +0200

    cambiato css su star rating

--- assets/js/star.js
+++ assets/js/star.js
@@ -1,10 +1,14 @@
 const target = document.getElementById('rating')
 const stars = target.querySelectorAll('svg')
+const starsPath = target.querySelectorAll('path')
 const radio = target.querySelectorAll('input[type=radio]')
 
 radio.forEach(el => {
     el.classList.add('hidden')
 })
 
-
-
+starsPath.forEach(star => {
+    star.addEventListener('mouseover', () => {
+        star.classList.toggle('activeStar')
+    })
+})
