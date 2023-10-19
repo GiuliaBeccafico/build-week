d6760841ae7d4144804ff36c4d16b9331769ac04
samuelet23 <samueletoscano@hotmail.com>
2023-10-19 17:06:02 +0200

    sweet alert

--- assets/js/star.js
+++ assets/js/star.js
@@ -42,3 +42,21 @@ for (let i = 0; i < starPath.length; i++){
 //             this.classList.remove('emptyStar')
 //         } else this.classList.add('emptyStar')
 // }
+
+let buttonFeedback = document.querySelector('.footer .blueButton')
+
+buttonFeedback.addEventListener('click', function () {
+    Swal.fire({
+        title: 'La Build-Week non è per niente stressante.Lo afferma Davide, 30 anni',
+        text: 'Scherziamo, dai!! Ne ha 25',
+        imageUrl: 'https://i.imgflip.com/2jsesx.jpg',
+        imageWidth: 480,
+        imageHeight: 414,
+        imageAlt: 'Custom image',
+      })
+})
+
+
+
+
+
