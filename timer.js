f23ff1001435151e976cb38623d9b93264ebf734
samuelet23 <samueletoscano@hotmail.com>
2023-10-19 14:20:13 +0200

    timer nella question page

--- timer.js
+++ timer.js
@@ -1,35 +1,4 @@
 
-
-let areaCheckbox = document.getElementById('checkbox');
-let areaButton = document.querySelector('.button');
-
-let firstPage = document.getElementById('page1')
-let secondPage = document.getElementById('page2')
-
-let timer = document.querySelector('.base-timer')
-areaButton.addEventListener('click', function () {
-  if (areaCheckbox.checked) {
-    firstPage.remove()
-    secondPage.classList.remove('hidden')
-    timer.classList.remove('hidden')
-    startTimer()
-  } else {
-    // L'utente non ha selezionato la casella di controllo, mostra un messaggio di avviso
-    Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: 'Please accept the agreement to proceed'
-    });
-  }
-});
-
-// areaButton.addEventListener ('click',startTimer())
-
-
-
-
-
-
 let timerContent = document.getElementById('timer')
 
 
@@ -101,34 +70,4 @@ function calculateTimeFraction() {
   return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
 }
 calculateTimeFraction();
-
-
-
-
-
-
-
-
-
-
-//domande easy 
-fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
-  .then(res => res.json())
-  .then(domande => {
-    let easy = domande.results //tutte le domande in questo array
-    let template = document.getElementsByTagName('template')[0];
-    console.log(easy);
-  })
-
-
-//domande hard
-fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=hard')
-  .then(res => res.json())
-  .then(domande => {
-    let hard = domande.results //tutte le domande in questo array
-    console.log(hard);
-  })
-
-
-
 
