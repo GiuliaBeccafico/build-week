972fe75c91308180a38e96732c4820c499bd0eed
GiuliaBeccafico <beccaficogiulia@hotmail.com>
2023-10-19 16:02:50 +0200

    test feedback value

--- assets/js/star.js
+++ assets/js/star.js
@@ -1,6 +1,8 @@
 const target = document.getElementById('rating')
 const starPath = target.querySelectorAll('path')
 const radio = target.querySelectorAll('input[type=radio]')
+const salvaButton = document.getElementById('salvaButton');
+const commentsBox = document.getElementById('commentsBox');
 
 
 
@@ -28,3 +30,48 @@ function starFiller(index, arr) {
             arr[j].classList.remove('emptyStar')
         } else arr[j].classList.add('emptyStar')
 }
+
+/********** Salvataggio valori nel bottone a sinistra */
+
+// Definisci un oggetto per salvare i dati
+let data = {
+    rating: null,
+    comment: ""
+};
+
+// Gestisci l'evento click del pulsante "Salva commento" e l'evento di input nel campo di commento
+salvaButton.addEventListener('click', () => {
+    const selectedRating = document.querySelector('input[name="rating"]:checked');
+    const comment = commentsBox.value.trim();
+    if (!selectedRating) {
+        alert('Inserisci la tua valutazione.');
+    } else if (comment === '') {
+        alert('Inserisci il tuo commento.');
+    } else {
+        // Salva i dati nel tuo sistema o esegui altre azioni necessarie
+        data.rating = selectedRating.value;
+        data.comment = comment;
+        // Converti l'oggetto data in una stringa JSON e salvala nel compartimento
+        const jsonData = JSON.stringify(data);
+        // Salva la stringa JSON nel compartimento o esegui altre azioni necessarie
+        Swal.fire('La tua valutazione è stata inviata!');
+        commentsBox.value = '';// Pulisci il campo di commento
+
+        // Pulisci la selezione delle stelle
+        const stars = document.querySelectorAll('input[name="rating"]');
+        stars.forEach(star => {
+            star.checked = false;
+        });
+
+        // Disabilita il pulsante "Salva commento" dopo l'invio
+        salvaButton.disabled = true;
+    }
+});
+
+commentsBox.addEventListener('input', () => {
+    if (commentsBox.value.trim() !== '' && document.querySelector('input[name="rating"]:checked')) {
+        salvaButton.disabled = false;
+    } else {
+        salvaButton.disabled = true;
+    }
+});
