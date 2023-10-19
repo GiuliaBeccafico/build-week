69ea5c40638356263c0de4a8412875d1682ea257
Maudit00 <davide9311@gmail.com>
2023-10-19 18:12:44 +0200

    question try 3

--- assets/js/questionTemplate.js
+++ assets/js/questionTemplate.js
@@ -1,69 +1,89 @@
 const proceedButton = document.getElementById('proceed')
-
+const header = document.querySelector('.header')
+const footer = document.querySelector('.footer')
+header.style.display = 'none'
+footer.style.display = 'none'
 proceedButton.addEventListener('click', showQuestions)
-const main = document.querySelector('.main')
-function showQuestions () {
+
+function showQuestions() {
+    header.style.display = 'flex'
+    footer.style.display = 'flex'
+    const nextButton = document.getElementById('nextButton')
+    const wrap = document.getElementById('contentWrap')
     fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
     .then(res => res.json())
     .then(domande => {
-    let easy = domande.results //tutte le domande in questo array
-    let temp = document.getElementsByTagName('template')[0]
-    let container = document.getElementById('container')
-    cloneSection(0, container, temp)
-    cloneSection(1, container, temp)
-    cloneSection(2, container, temp)
-    questionTitles(easy)
-    // questionTitles()
-    // answerMaker(4)
-    // const button = document.getElementById('nextButton')
-    // button.addEventListener('click', ()=> {
-    // nextQuestion(document.querySelector('.questions', container, temp))
-  })
-    
+        let easy = domande.results //tutte le domande in questo array
+        const [a, b, c] = easy 
+        console.log(a['correct_answer'], b, c);
+        cloneTemplate(wrap)
+        const answerArea = document.querySelector('#contentWrap').children[0].lastElementChild
+            questionTitles(easy, 0, wrap)
+            answerMaker(answerArea, easy)
+            
+            nextButton.addEventListener('click', () => {
+                    clearQuestions(wrap)
+                    clearQuestions(answerArea)
+                    cloneTemplate(wrap)
+                    questionTitles(easy, 2, wrap)
+                    answerMaker(answerArea, easy)
+                
+                })
+        })
+}
+
+function cloneTemplate(target) {
+    const temp = document.getElementById('questionsTemplate')
+    const content1 = temp.content.cloneNode(true)
+    target.append(content1)
 }
 
 
-// function answerCheck (){
-//     const target = document.querySelector('.answ')
-// }
 
-function nextQuestion (target, container, temp){
-    console.log(target, document.querySelector('.main'));
-    target.innerHTML = ''
-//    cloneSection(0, container, temp)
-//    cloneSection(1, container, temp)
-//    cloneSection(2, container, temp)
-//    questionTitles()
-//    answerMaker(2)
+
+function clearQuestions(target) {
+    target.innerText = ''
 }
 
-function cloneSection (index, target, temp){
+function cloneSection(index, target, temp) {
     const children = temp.content.children[index].cloneNode(true)
     target.append(children)
 }
 
-function questionTitles (questions){
-    const main = document.querySelector('.main')
-    let firstTitle = main.children[0].children[0]
-    let secondTitle = main.children[0].children[1]
-    let question = questions[0].question.split(' ')
-    let first = question.slice(0, question.length/2).join(' ')
-    let second = question.slice(question.length/2, question.length).join(' ')
-    firstTitle.innerText = first
-    secondTitle.innerText = second
+function questionTitles(questions, index, target) {
+    let firstTitle = target.children[0].children[0]
+    let secondTitle = target.children[0].children[1]
+    let question = questions[index].question.split(' ')
+    let first = question.slice(0, question.length / 2).join(' ')
+    let second = question.slice(question.length / 2, question.length).join(' ')
+    firstTitle.innerHTML = first
+    secondTitle.innerHTML = second
 }
 
-function answerMaker (n){
-    const target = document.querySelector('.main').children[0].lastElementChild
-    for (let i = 1; i <= n; i++){
-    let answer = document.createElement('button')
-    answer.classList.add('answer')
-    answer.innerText = i
-    target.append(answer)
+function answerMaker(target, questions) {
+    
+    let arr = [questions[0]['correct_answer']]
+    arr = arr.concat(questions[0]['incorrect_answers'])
+    console.log(arr);
+    shuffleArray(arr)
+    for (let i = 0; i <= arr.length-1; i++) {
+        let answer = document.createElement('button')
+        answer.classList.add('answer')
+        answer.innerHTML = arr[i]
+        target.append(answer)
     }
-
 }
 
+function clearQuestions (target) {
+target.innerHTML = ''
+}
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 // function disableButton (){
 //     document.getElementById('nextButton').disabled = true
