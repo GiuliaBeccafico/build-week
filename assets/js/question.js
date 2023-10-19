def39ac5b8dde7931fff5cb974830c46dd62fa2a
Maudit00 <davide9311@gmail.com>
2023-10-19 12:28:54 +0200

    question append succed

--- assets/js/question.js
+++ assets/js/question.js
@@ -1,18 +1,18 @@
-fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
-  .then(res => res.json())
-  .then(domande => {
-    let easy = domande.results //tutte le domande in questo array
-    let template = document.getElementsByTagName('template')[0];
-    
-    const main = document.querySelector('.main')
-    let firstTitle = main.children[0].children[0]
-    let secondTitle = main.children[0].children[1]
-    let question = easy[0].question.split(' ')
-    let first = question.slice(0, question.length/2).join(' ')
-    let second = question.slice(question.length/2, question.length).join(' ')
-    firstTitle.innerText = first
-    secondTitle.innerText = second
-  })
+// fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
+//   .then(res => res.json())
+//   .then(domande => {
+//     let easy = domande.results //tutte le domande in questo array
+//     let template = document.getElementsByTagName('template')[0];
+//     console.log(template);
+//     // const main = document.querySelector('.main')
+//     // let firstTitle = main.children[0].children[0]
+//     // let secondTitle = main.children[0].children[1]
+//     // let question = easy[0].question.split(' ')
+//     // let first = question.slice(0, question.length/2).join(' ')
+//     // let second = question.slice(question.length/2, question.length).join(' ')
+//     // firstTitle.innerText = first
+//     // secondTitle.innerText = second
+//   })
 
 //prove attach questions
 
--- assets/js/questionTemplate.js
+++ assets/js/questionTemplate.js
@@ -3,24 +3,29 @@ const proceedButton = document.getElementById('proceed')
 proceedButton.addEventListener('click', showQuestions)
 const main = document.querySelector('.main')
 function showQuestions () {
+    fetch('https://opentdb.com/api.php?amount=10&category=18&difficulty=easy')
+    .then(res => res.json())
+    .then(domande => {
+    let easy = domande.results //tutte le domande in questo array
     let temp = document.getElementsByTagName('template')[0]
     let container = document.getElementById('container')
     cloneSection(0, container, temp)
     cloneSection(1, container, temp)
     cloneSection(2, container, temp)
-    questionTitles()
-    answerMaker(4)
-    const button = document.getElementById('nextButton')
-    button.addEventListener('click', ()=> {
-    nextQuestion(document.querySelector('.questions', container, temp))
-    answerCheck
-})
+    questionTitles(easy)
+    // questionTitles()
+    // answerMaker(4)
+    // const button = document.getElementById('nextButton')
+    // button.addEventListener('click', ()=> {
+    // nextQuestion(document.querySelector('.questions', container, temp))
+  })
+    
 }
 
-function answerCheck (){
-    const target = document.querySelectorAll('.answer')
-    console.log('.answer');
-}
+
+// function answerCheck (){
+//     const target = document.querySelector('.answ')
+// }
 
 function nextQuestion (target, container, temp){
     console.log(target, document.querySelector('.main'));
@@ -37,13 +42,15 @@ function cloneSection (index, target, temp){
     target.append(children)
 }
 
-function questionTitles (){
+function questionTitles (questions){
     const main = document.querySelector('.main')
     let firstTitle = main.children[0].children[0]
-    firstTitle.innerText = 'prima domanda'
     let secondTitle = main.children[0].children[1]
-    secondTitle.innerText = 'seconda domanda'
-    console.log(main);
+    let question = questions[0].question.split(' ')
+    let first = question.slice(0, question.length/2).join(' ')
+    let second = question.slice(question.length/2, question.length).join(' ')
+    firstTitle.innerText = first
+    secondTitle.innerText = second
 }
 
 function answerMaker (n){
