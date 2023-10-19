13f04acb20e513919130d22af86ab7122176fbf8
samuelet23 <samueletoscano@hotmail.com>
2023-10-19 19:16:17 +0200

    aggiunta bottone valutazione

--- assets/js/star.js
+++ assets/js/star.js
@@ -1,7 +1,9 @@
 const target = document.getElementById('rating')
-const starPath = document.querySelectorAll('path')
-const radio = document.querySelectorAll('input[type=radio]')
-log
+const starPath = target.querySelectorAll('path')
+const radio = target.querySelectorAll('input[type=radio]')
+const salvaButton = document.getElementById('salvaButton');
+const commentsBox = document.getElementById('commentsBox');
+const ratingContainer = document.getElementById('rating');
 
 
 
@@ -10,7 +12,7 @@ radio.forEach(el => {
 })
 
 
-/** funzionante/** */
+/*********** funzionante */
 for (let i = 0; i < starPath.length; i++) {
     starPath[i].index = i
     starPath[i].addEventListener('click', () => {
@@ -29,20 +31,51 @@ function starFiller(index, arr) {
             arr[j].classList.remove('emptyStar')
         } else arr[j].classList.add('emptyStar')
 }
-let buttonFeedback = document.querySelector('.footer .blueButton')
 
-buttonFeedback.addEventListener('click', function () {
-    Swal.fire({
-        title: 'La Build-Week non è per niente stressante.Lo afferma Davide, 30 anni',
-        text: 'Scherziamo, dai!! Ne ha 25',
-        imageUrl: 'https://i.imgflip.com/2jsesx.jpg',
-        imageWidth: 480,
-        imageHeight: 414,
-        imageAlt: 'Custom image',
-      })
-})
+/********** Salvataggio valori nel bottone a sinistra */
+
+// Definisci un oggetto per salvare i dati
+let data = {
+    rating: null,
+    comment: ""
+};
+
+// gestisce l'evento click del pulsante "Salva commento" e l'evento di input nel campo di commento
+salvaButton.addEventListener('click', () => {
+    const selectedRating = document.querySelector('input[name="rating"]:checked');
+    const comment = commentsBox.value.trim();
+    if (!selectedRating) {
+        alert('Inserisci la tua valutazione.');
+    } else if (comment === '') {
+        alert('Inserisci il tuo commento.');
+    } else {
+        
+        data.rating = selectedRating.value;
+        data.comment = comment;// Converte l'oggetto data in una stringa JSON e salvala
+        
+        const jsonData = JSON.stringify(data);// Salva la stringa JSON
+        
+        Swal.fire('La tua valutazione è stata inviata!');
+        commentsBox.value = '';// Pulisci il campo di commento
+        resetStelle();
 
 
+        
+        salvaButton.disabled = true; // Disabilita il pulsante "Salva commento" dopo l'invio
+    }
+});
 
+function resetStelle() {
+    // Deseleziona tutte le stelle
+    starPath.forEach(star => {
+        star.classList.add('emptyStar');
+    });
+}
 
-
+commentsBox.addEventListener('input', () => {
+    if (commentsBox.value.trim() !== '' && document.querySelector('input[name="rating"]:checked')) {
+        salvaButton.disabled = false;
+    } else {
+        salvaButton.disabled = true;
+    }
+});
