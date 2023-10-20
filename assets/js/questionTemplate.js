de440c59b2a4499f6ce9ea5fb23e239a9d4f6c8b
samuelet23 <samueletoscano@hotmail.com>
2023-10-20 09:01:57 +0200

    commentato welcome page js

--- assets/js/questionTemplate.js
+++ assets/js/questionTemplate.js
@@ -111,64 +111,85 @@ function answerMaker (n){
 
 // js-script inizio
 
+// timer js inizio
 
-let timerContent = document.getElementById('timer') //sto selezionado la parte dove verrà inserito il numero del timer
-let cerchio = document.querySelector(".base-timer"); 
+// Seleziona l'elemento HTML con l'ID "timer" dove verrà inserito il numero del timer
+let timerContent = document.getElementById('timer');
+
+// Seleziona l'elemento HTML con la classe "base-timer", che contiene il timer
+let cerchio = document.querySelector(".base-timer");
+
+// Seleziona l'elemento "path" all'interno del cerchio con l'ID "base-timer-path-remaining"
 let PercorsoSecondoCerchio = document.getElementById("base-timer-path-remaining");
+
+// Definisci la costante "raggio" per il cerchio del timer
 const raggio = 45;
+
+// Calcola la lunghezza della circonferenza del cerchio
 const lunghezzaCirconferenza = 2 * Math.PI * raggio;
+
+// Costante per la lunghezza massima della "stroke-dasharray"
 const FULL_DASH_ARRAY = lunghezzaCirconferenza;
+
+// Imposta il limite di tempo del timer in secondi (in questo caso, 60 secondi)
 const TIME_LIMIT = 60;
+
+// Variabile per tenere traccia del tempo trascorso
 let timePassed = 0;
+
+// Variabile per tenere traccia del tempo rimanente
 let timeLeft = TIME_LIMIT;
+
+// Inizializza il contenuto dell'elemento "timer" con il tempo rimanente
 timerContent.innerText = timeLeft;
+
+// Variabile per memorizzare l'intervallo del timer
 let timerInterval = null;
 
-
-
-//
+// Funzione per formattare il tempo rimanente in formato "minuti:secondi"
 function formatTimeLeft(time) {
   const minutes = Math.floor(time / 60);
   let seconds = time % 60;
   if (seconds < 10) {
-    return (seconds = `0${seconds}`);
+    seconds = `0${seconds}`;
   }
   return `${minutes}:${seconds}`;
 }
 
-
-//
+// Funzione per avviare il timer
 function startTimer() {
-  if (timerInterval !== null) { // Controlla se il timer non è già in esecuzione
-    clearInterval(timerInterval); // Ferma il timer se è in esecuzione
+  if (timerInterval !== null) {
+    clearInterval(timerInterval); // Ferma il timer se è già in esecuzione
   }
   timePassed = 0;
   timeLeft = TIME_LIMIT;
   timerContent.innerText = timeLeft;
 
-  setCircleDasharray();
+  setCircleDasharray(); // Imposta la lunghezza della "stroke-dasharray" del cerchio
 
+  // Avvia l'intervallo del timer
   timerInterval = setInterval(() => {
-    timePassed = timePassed += 1;
+    timePassed += 1;
     timeLeft = TIME_LIMIT - timePassed;
-
     timerContent.innerText = timeLeft;
 
-    setCircleDasharray();
+    setCircleDasharray(); // Aggiorna la "stroke-dasharray" del cerchio
 
     if (timeLeft <= 0) {
-      clearInterval(timerInterval); // Ferma il timer quando raggiunge 0
+      clearInterval(timerInterval); // Ferma il timer quando il tempo rimanente raggiunge 0
     }
   }, 1000);
 }
+
+// Avvia il timer quando la pagina si carica
 startTimer();
 
-
-//
+// Funzione per calcolare la frazione di tempo rimanente rispetto al limite di tempo
 function calculateTimeFraction() {
   return timeLeft / TIME_LIMIT;
 }
 
+// Funzione per impostare la "stroke-dasharray" del cerchio in base alla frazione di tempo rimanente
 function setCircleDasharray() {
   const circleDasharray = `${(
     calculateTimeFraction() * FULL_DASH_ARRAY
@@ -176,8 +197,5 @@ function setCircleDasharray() {
   PercorsoSecondoCerchio.setAttribute("stroke-dasharray", circleDasharray);
 }
 
-function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / TIME_LIMIT;
-  return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
-}
+// Calcola la frazione di tempo iniziale quando la pagina si carica
 calculateTimeFraction();
